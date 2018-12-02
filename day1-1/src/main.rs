090b0d87a2100f1aa887b61a04a2bc3971a5ba7e
John Vivian <jtvivian@gmail.com>
2018-12-01 22:27:47 -0800

    Rewrite idiomatically
    Thanks to burntsushi

--- day1-1/src/main.rs
+++ day1-1/src/main.rs
@@ -1,5 +1,5 @@
-use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{self, Result, Read, Write};
+type Result<T> = Result<T, Box<std::error::Error>>;
 
 /// After feeling like you've been falling for a few minutes, you look at the device's tiny screen.
 /// "Error: Device must be calibrated before first use. Frequency drift detected. Cannot maintain
@@ -9,29 +9,23 @@ use std::io::{BufRead, BufReader};
 ///
 /// Starting with a frequency of zero, what is the resulting frequency after all of the changes in
 /// frequency have been applied?
-fn main() {
 
-    // Open file and create BufReader object which allows iteration over lines
-    let f = File::open("input")
-        .expect("Failed to open file");
-    let f = BufReader::new(f);
 
-    // Declare signed counter since we have both negative and positive values
-    let mut counter: i32 = 0;
 
-    // Iterate over lines and add/subtract based on first char in string
-    for line in f.lines() {
-        let line = line.expect("Failed to read line");
+fn main() -> Result<()> {
 
-        // Extract and declare integer type here as compiler cannot infer type
-        let val: i32 = line[1..].parse().unwrap();
+    let mut freq = 0;
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
 
-        if line.chars().next().unwrap() == '+' {
-            counter += val;
-        } else {
-            counter -= val;
-        }
+    for line in input.lines() {
+        let value: i32 = line.parse()?;
+        freq += value;
     }
 
-    println!("Counter Value: {}", counter);
+    writeln!(io::stdout(), "Frequency is: {}", freq)?;
+    Ok(freq)
 }
+
+
+
